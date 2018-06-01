9979482e62f304cdec2eb5cc4df6b212625be409
cserli <1528884535@qq.com>
2018-06-01 17:47:01 +0800

    add

---
+++ src/FenDZ/go-entitas-master/compoentdata.go
@@ -0,0 +1,32 @@
+package entitas
+
+// Component index
+// 组件--也就相当于数据块
+const (
+	ComponentA ComponentType = iota
+	ComponentB
+	ComponentC
+	ComponentD
+	ComponentE
+	ComponentF
+	NumComponents
+)
+
+// 数据快的解释
+// --- A ----------------------------------------------------------------------
+
+type componentA struct {
+	value int
+}
+
+func NewComponentA(value int) Component    { return &componentA{value: value} }
+func (c1 *componentA) Type() ComponentType { return ComponentA }
+func (c1 *componentA) String() string      { return "A" }
+
+// --- B ----------------------------------------------------------------------
+
+type componentB struct{ value float32 }
+
+func NewComponentB(value float32) Component { return &componentB{value: value} }
+func (c2 *componentB) Type() ComponentType  { return ComponentB }
+func (c2 *componentB) String() string       { return "B" }
--- src/lollipopGoECS/tmain.go
+++ src/lollipopGoECS/tmain.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"FenDZ/go-entitas-master" // ECS的基础架构方式
 	"fmt"
 )
 
 func init() {
 	fmt.Println("init ECS")
+	// 创建一个实体
+	e := entitas.NewEntity(0)
+	// 创建一个组件
+	c1 := entitas.NewComponentA(1)
+	c2 := entitas.NewComponentB(1.0)
+	// 组件的类型
+	types := []entitas.ComponentType{c1.Type(), c2.Type()}
+	fmt.Println(types)
+	// 加入组件
+	e.AddComponent(c1)
 	return
 }
 
